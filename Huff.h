a7ddedfa28b6ceb2fd7a59c6288abbc645d52c5d
Gabriel Huff <74307411+Gabrielhuff47@users.noreply.github.com>
2020-11-21 20:37:20 -0300

    Add files via upload

---
+++ Huff.h
@@ -0,0 +1,13 @@
+#ifndef HUFF_H_INCLUDED
+#define HUFF_H_INCLUDED
+
+#define MAX 10
+
+int maiorvalor(int a, int b);
+int EscreverDados(int a, int b, int c);
+float MediaPorSet(int a, int b);
+void ordenaDecrescente(float *vetor2,int numdejogador);
+void mostrardados(int *vetor, float *vetor2,int numdejogador);
+
+#endif /* HUFFS_H_INCLUDED */
+
---
+++ Main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "lib.h"
+#define MAXSCORE 5 
+
+ main (){
+ 	int i,condi,y,c,x,erros,cont,a,b,set,aux=0,numdejogador;
+	int vetor[10], vetor3[10];
+	float vetor2[10];
+  		printf("Digite o Numero de jogadores:");
+    	scanf("%d",&numdejogador);
+	  		for( i=0;i<numdejogador;i++){ 
+				printf("Digite os dados do jogador : pontos de ataque/saque/ bloqueios:"); 
+				scanf("%d%d%d",&a,&b,&c);
+ 				printf("Digite o numero de Set:"); 
+ 				scanf("%d",&set); 
+ 	 			vetor[i]=EscreverDados(a,b,c);
+  	  			vetor2[i]=MediaPorSet(EscreverDados(a,b,c),set);	  	 
+			}
+mostrardados(vetor,vetor2,numdejogador);
+ordenaDecrescente(vetor2,numdejogador);
+}
+
